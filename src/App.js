c179e8e9d9f9232f25453a5f6e27f172f654a3f1
Joseph Soria <josephsoria6@gmail.com>
2022-03-24 22:23:41 -0500

    Styled calculator shape and colors

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div >
-      <header>
-        <h1>This is a calculator</h1>
-      </header>
+    <div className="container-fluid">
+        <div className="row mt-5 justify-content-center">
+          <div className="main-background">
+            <div className="secondary-background">
+              <h2>this is the secondary background</h2>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }
