39dfb246b6f34f28848a84ea1028010aa70119ff
Joseph Soria <josephsoria6@gmail.com>
2022-03-24 23:15:30 -0500

    Imported validator function

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import inputValidation from "../utils/helper/input-validation";
 import "./Screen.css"
 
 const Screen = () => {
+  const [ input, setInput ] = useState("");
+
+  const onInputChange = (e) => {
+    const validatedString = inputValidation(e.target.value);
+    setInput(validatedString);
+  }
+
   return (
     <form type="submit">
       <input
@@ -9,6 +17,8 @@ const Screen = () => {
         type="text"
         id="screen"
         name="screen"
+        value={input}
+        onChange={(e) => onInputChange(e)}
       />
     </form>
   );
