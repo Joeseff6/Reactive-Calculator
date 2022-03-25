db39474b31c4d2ac75517713172e04f9d8ae4a82
Joseph Soria <josephsoria6@gmail.com>
2022-03-24 22:47:55 -0500

    Created and imported Screen component, styled Screen

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
+import Screen from "./components/Screen";
 import "./App.css";
 
 function App() {
   return (
     <div className="container-fluid">
-        <div className="row mt-5 justify-content-center">
-          <div className="main-background">
-            <div className="secondary-background">
-              <div className="row">
-                <div className="col">
-                  <h6 className="text-center calculator-model mt-2" >Reactive Calculator v1.0</h6>
-                </div>
+      <div className="row mt-5 justify-content-center">
+        <div className="main-background">
+          <div className="secondary-background">
+            <div className="row">
+              <div className="col">
+                <h6 className="text-center calculator-model mt-2">
+                  Reactive Calculator v1.0
+                </h6>
               </div>
+            </div>
+            <div className="row justify-content-center mt-2">
+              <Screen />
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Screen.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Screen.css"
+
+const Screen = () => {
+  return (
+    <form type="submit">
+      <input
+        className="form-control"
+        type="text"
+        id="screen"
+        name="screen"
+      />
+    </form>
+  );
+};
+
+export default Screen;
