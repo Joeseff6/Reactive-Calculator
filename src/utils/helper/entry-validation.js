1df7085d90882ba2f1ff827376e3e34c7937d738
Joseph Soria <josephsoria6@gmail.com>
2022-04-03 15:45:13 -0500

    Replaced all button variables with input, changed export statement and function name

--- src/utils/helper/entry-validation.js
+++ src/utils/helper/entry-validation.js
@@ -1,11 +1,36 @@
-const entryValidation = (event) => {
-  console.log(event)
-  if (event.target.value[0] === "0" && event.nativeEvent.data === "0") {
-    return 0;
-  } else {
-    const regex = /[^0-9+*-/=.]/g
-    return event.target.value.replace(regex,"");
-  } 
+const entryValidation = (input, entry, result) => {
+  let newEntry = "";
+  let newResult = result ? result : "";
+  if ((input === "." && entry.includes("."))
+    || (entry === "0" && input === "0")
+    || (input === "neg" && entry === "0")
+    || (input === "neg" && !entry)
+    || (/[\+÷x-]/.test(input) && !entry && !result)
+  ) return;
+  if (/[0-9]/.test(input)) newEntry = entry + input;
+  if (/[1-9]/.test(input) && entry === "0") newEntry = input;
+  if ((input === "." && entry === "0")
+    || (input === "." && !entry)
+    ) {
+      newEntry = "0.";
+    } else if (input === "." && entry !== "0") {
+      newEntry = entry + input;
+    }
+  if (input === "neg" && entry !== "0" && !/-/.test(entry)) {
+    newEntry = `(-${entry})`;
+  } else if (input === "neg" && entry !== "0" && /-/.test(entry)) {
+    newEntry = entry.replace(/[()-]/g,"");
+  }
+  if (/[()]/.test(entry) && /[0-9]/.test(input)) newEntry = entry.slice(0,-1) + input + ")";
+  if (/[\+÷x-]/.test(input) && entry && !result) {
+    newResult = `${entry} ${input}`;
+  } else if (/[\+÷x-]/.test(input) && entry && result) {
+    newResult = `${result} ${entry} ${input}`;
+  } else if (/[\+÷x-]/.test(input) && !entry && result) {
+    newResult = `${result.slice(0,-2)} ${input}`;
+  }
+
+  return [newEntry, newResult];
 }
 
 export default entryValidation;
