b6989a616b412f5194820902d76ea5a16c781dc7
Joseph Soria <josephsoria6@gmail.com>
2022-04-10 12:45:58 -0500

    Added conditions for negatvie numebrs

--- src/utils/helper/insert-commas.js
+++ src/utils/helper/insert-commas.js
@@ -1,9 +1,11 @@
 const insertCommas = (string) => {
   if (string.length < 4) return string;
-  let reversedString = string.split("").reverse().join("");
-  let counter = 0
+  let stringCopy = string;
+  if (string[0] === "-") stringCopy = stringCopy.slice(1,stringCopy.length);
+  let reversedString = stringCopy.split("").reverse().join("");
+  let counter = 0;
   let stringWithCommas = "";
-  for (let i = 0; i < string.length; i++) {
+  for (let i = 0; i < reversedString.length; i++) {
     counter++;
     if ((counter === 3) && ( i !== reversedString.length - 1)) {
       stringWithCommas += reversedString[i] + ",";
@@ -12,7 +14,7 @@ const insertCommas = (string) => {
       stringWithCommas += reversedString[i];
     }
   }
-  return stringWithCommas.split("").reverse().join("");
+  return string[0] === "-" ? "-" + stringWithCommas.split("").reverse().join("") : stringWithCommas.split("").reverse().join("");
 }
 
 export default insertCommas;
