7351a55205a20c4d351ad9361e02e30daa1adac2
Joseph Soria <josephsoria6@gmail.com>
2022-04-10 13:29:49 -0500

    Removed entry prop, added PropTypes

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
+import PropTypes from "prop-types";
 import insertCommas from "../utils/helper/insert-commas";
 import "./Screen.css";
 
-const Screen = ({entry, result, ans }) => {
+const Screen = ({result, ans }) => {
   useEffect(() => {
     document.getElementById("screen").focus();
-  }, [entry, result]);
+  }, [result]);
+
+  console.log(typeof(result))
 
   return (
     <div className="mb-2" id="screen">
@@ -14,5 +17,10 @@ const Screen = ({entry, result, ans }) => {
     </div>
   );
 };
+
+Screen.propTypes = {
+  result: PropTypes.string,
+  ans: PropTypes.string,
+}
 
 export default Screen;
