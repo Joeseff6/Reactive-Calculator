8780fa08c3789c7acedebeadb44d1046f0ba803f
Joseph Soria <josephsoria6@gmail.com>
2022-04-10 12:36:06 -0500

    Wrote logic

--- src/utils/helper/insert-commas.js
+++ src/utils/helper/insert-commas.js
@@ -1,5 +1,18 @@
 const insertCommas = (string) => {
-
+  if (string.length < 4) return string;
+  let reversedString = string.split("").reverse().join("");
+  let counter = 0
+  let stringWithCommas = "";
+  for (let i = 0; i < string.length; i++) {
+    counter++;
+    if ((counter === 3) && ( i !== reversedString.length - 1)) {
+      stringWithCommas += reversedString[i] + ",";
+      counter = 0;
+    } else {
+      stringWithCommas += reversedString[i];
+    }
+  }
+  return stringWithCommas.split("").reverse().join("");
 }
 
 export default insertCommas;
